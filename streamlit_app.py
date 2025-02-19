41839061bc4a41314f3551fa63aa650a764b3b1d
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2025-02-19 08:20:26 +0000

    adf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,5 @@
 import streamlit as st
 
-st.title('エラトステネスの篩')
+st.title('**素数判定')
 
 n = st.number_input('nの値を入力してください' , min_value = 0)
-
-'''def prime():
-    if n % 10 != 4 or n % 10 != 6 or n % 10 != 0:
-        st.write('No')
-    else:
-        st.write('Yes')
-
-if st.button('実行'):
-    prime()'''
