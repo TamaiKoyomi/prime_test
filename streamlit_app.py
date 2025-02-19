6f6e56fe6064acbf3c0f684db7254e5149abf342
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2025-02-19 08:16:27 +0000

    asfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.title('エラトステネスの篩')
 
 n = st.number_input('nの値を入力してください' , min_value = 0)
 
-def prime():
+'''def prime():
     if n % 10 != 4 or n % 10 != 6 or n % 10 != 0:
         st.write('No')
     else:
         st.write('Yes')
 
 if st.button('実行'):
-    prime()
+    prime()'''
