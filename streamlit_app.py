578ad61479ea85b89800535e9dd90169e9cfc0ad
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2025-02-19 08:14:17 +0000

    ad

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,24 +4,8 @@ st.title('エラトステネスの篩')
 
 n = st.number_input('nの値を入力してください' , min_value = 0)
 
-origin = []
-
-for i in range(10 ** 60):
-    if i != 1:
-        origin.append(i)
-
-j = 2
-
-while j > 10 ** 60 / 2:
-    if j == origin[0]:
-        for k in origin:
-            if origin[k] % j == 0:
-                origin.pop(k)
-
-st.write(origin)
-
 def prime():
     if n % 10 != 4 or n % 10 != 6 or n % 10 != 0:
         st.write('No')
     else:
-        pass
+        st.write('Yes')
