2514e0afca2cad20766728752da421a6d293ee88
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2025-02-20 08:24:08 +0000

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,24 @@
 import streamlit as st
+import math
 
 st.title('ぱう')
 
-#n = st.number_input('nの値を入力してください' , min_value = 0)
+n = st.number_input('nの値を入力してください' , min_value = 0)
 
-st.write('そんなものはなかった')
+def is_prime_optimized(n):
+    if n <= 1:
+        return False
+    if n <= 3:  # 2と3は素数
+        return True
+    if n % 2 == 0 or n % 3 == 0:  # 2と3の倍数は素数でない
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+# 使用例
+st.write(is_prime_optimized(29))  # 結果: True
+st.write(is_prime_optimized(49))  # 結果: False
